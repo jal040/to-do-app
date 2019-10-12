4a30072f18a84730d7b2b023d6b80025baa59c3c
Jamie <jamielee3136@gmail.com>
2019-10-12 13:41:32 -0700

    Checkpoint 12 Assignment complete

--- app.js
+++ app.js
@@ -11,18 +11,48 @@ function onReady() {
 
     // create a new li
     let newLi = document.createElement('li');
+    newLi.classList.add("mdl-list__item");
 
     // create a new input
     let checkbox = document.createElement('input');
-
+    // add class to checkbox
+    checkbox.classList.add("mdl-checkbox__input");
     // set the input's type to checkbox
     checkbox.type = 'checkbox';
 
+    // create label for checkbox
+    let lb = document.createElement('label');
+    lb.className = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
+
+    lb.appendChild(checkbox);
+
+    // create delete button
+    let deletebtn = document.createElement('button');
+    deletebtn.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent";
+
+    // create span class
+    let sp1 = document.createElement('span');
+    let sp2 = document.createElement('span');
+    sp1.classList.add("mdl-list__item-primary-content");
+    sp2.classList.add("mdl-list__item-secondary-action");
+    sp2.appendChild(lb);
+
+
     // set the title
-    newLi.textContent = title;
+    // newLi.textContent = title;
+    sp1.textContent = title;
+    deletebtn.textContent = "Delete";
+
+    // add event to delete
+    deletebtn.addEventListener("click", function(event){
+      toDoList.removeChild(this.parentElement);
+    });
 
     // attach the checkbox to the li
-    newLi.appendChild(checkbox);
+    newLi.appendChild(sp1);
+    newLi.appendChild(sp2)
+    //newLi.appendChild(checkbox);
+    newLi.appendChild(deletebtn);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
@@ -31,9 +61,25 @@ function onReady() {
     newToDoText.value = '';
   });
 
+
+
+}
+
+function deleteToDo() {
+  console.log('hi');
+  let arr = document.getElementById('toDoList');
+  let items = arr.getElementsByTagName('li');
+  console.log(items.length);
+  for( var i = 0; i < items.length; i++){
+    var chk = items[i].getElementsByTagName('input');
+    if(chk){
+      console.log(chk)
+      // arr.removeChild(arr.childNodes[i]);
+    }
+  }
 }
 
 window.onload = function() {
-  alert("The window has loaded!");
+  // alert("The window has loaded!");
   onReady();
 };
